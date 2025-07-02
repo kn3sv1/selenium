12ba673f6bdc3d6038d11a1b4cf3a987e6d88ca3
RSatanovskyi <satcms@hotmail.com>
2025-07-02 13:04:33 +0300

    CLI EXIT CODE SUPPORT

--- CalculatorFactoryMethod/src/main/CalculatorApp.java
+++ CalculatorFactoryMethod/src/main/CalculatorApp.java
@@ -1,22 +1,48 @@
 package main;
 
+import java.util.Scanner;
+
 public class CalculatorApp {
     public static void main(String[] args) {
 
         Calculator c = new Calculator();
         OperationFactory factory = new OperationFactory();
+
+        Scanner scanner = new Scanner(System.in);  // Create Scanner object
+
+        System.out.print("Enter operation: ");
+        String nameOperation = scanner.nextLine();          // Read line of input
+
+        System.out.print("First 1st number: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Enter 2nd number: ");
+        double b = scanner.nextDouble();
+
+        try {
+            c.setOperation(factory.getOperationByName(nameOperation));
+            System.out.println("Result: " + c.apply(a, b));
+            System.exit(0); // Normal exit
+        } catch (OperationException e) {
+            System.err.println("Your operation is WRONG: " + nameOperation);
+            System.err.println("Exiting with Error Code: 20");
+            System.exit(20); // Give code to Operation System
+        }
+
+
         // now entry point doesn't know which class will be used for operation.
         // we hided this information inside OperationFactory class
         // entry point now is clean and only knows about operation interface that returns OperationFactory class
 
         // looks like we still have a problem - it should know about words: subtract or add
         // it's temporary that we show it here. This word we get from terminal/CLI from user like numbers as well
-        Operation operation = factory.getOperationByName("subtract");
-        // Operation operation = factory.getOperationByName("addMinusOne");
 
-
-        c.setOperation(operation);
-        System.out.println("Result: " + c.apply(1, 3));
+//        Operation operation = factory.getOperationByName("subtract");
+//        // Operation operation = factory.getOperationByName("addMinusOne");
+//
+//
+//        c.setOperation(operation);
+//        System.out.println("Result: " + c.apply(1, 3));
 
 //        // just for fun - practice without extra variable operation. In real code we'll not use unnecessary variables
 //        c.setOperation(factory.getOperationByName("add"));
