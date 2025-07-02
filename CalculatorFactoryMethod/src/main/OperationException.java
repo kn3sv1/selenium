7178718b0d2f4f18e80d913c328f0c25ac957b9f
RSatanovskyi <satcms@hotmail.com>
2025-07-02 15:34:37 +0300

    CLI EXIT CODE SUPPORT

--- CalculatorFactoryMethod/src/main/OperationException.java
+++ CalculatorFactoryMethod/src/main/OperationException.java
@@ -1,6 +1,8 @@
 package main;
 
 public class OperationException extends RuntimeException {
+    // without arguments constructor calls itself bellow with an argument and a default message:
+    //"Operation class not found!"
     public OperationException() {
         this("Operation class not found!");
     }
