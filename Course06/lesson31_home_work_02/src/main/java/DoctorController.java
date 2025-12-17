73c62a597b4198557e0331baedc5b9c897ff91d8
RSatanovskyi <satcms@hotmail.com>
2025-12-17 15:35:02 +0200

    refactoring finished

--- Course06/lesson31_home_work_02/src/main/java/DoctorController.java
+++ Course06/lesson31_home_work_02/src/main/java/DoctorController.java
@@ -1,14 +1,11 @@
 import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class DoctorController extends AbstractController{
     private final DoctorService service;
-    private final SanitizerService sanitizer;
     private final DoctorValidator validator;
     private final DoctorView view;
 
@@ -16,7 +13,6 @@ public class DoctorController extends AbstractController{
         // in memory should be the same object that we use in controller because there should be one reference
         // of object in memory otherwise if we add to ArrayList from that instance another will not be updated.
         this.service = new DoctorService(new DoctorRepository());
-        this.sanitizer = new SanitizerService();
         this.validator = new DoctorValidator();
         // they don't have any state inside like repository so we can copy in memory and nothing will be broken.
         this.view = new DoctorView(new TemplateService(), new SanitizerService());
@@ -42,27 +38,18 @@ public class DoctorController extends AbstractController{
             // before we make decision to save we should validate and be sure that we don't have errors.
             Map<String, String> errors = this.validator.validate(formData);
             if (errors.isEmpty()) {
-                this.createIfSuccess(exchange, formData);
+                this.service.create(formData);
+                // redirect back to form page with success message
+                exchange.getResponseHeaders().add("Location", "/doctor/show-doctors");
+                exchange.sendResponseHeaders(303, -1); // 303 = See Other
+                exchange.close();
             } else  {
-                this.showCreateForm(exchange, formData, errors);
+                this.sendHTMLResponse(exchange, this.view.createForm(formData, errors));
             }
         } else {
             // first time we send request it will show an empty form without any errors or resubmitted data
-            this.showCreateForm(exchange, new HashMap<>(), new HashMap<>());
+            this.sendHTMLResponse(exchange, this.view.createForm(new HashMap<>(), new HashMap<>()));
         }
-    }
-
-    private void createIfSuccess(HttpExchange exchange, Map<String, String> formData) throws IOException {
-        this.service.create(formData);
-
-        // redirect back to form page with success message
-        exchange.getResponseHeaders().add("Location", "/doctor/show-doctors");
-        exchange.sendResponseHeaders(303, -1); // 303 = See Other
-        exchange.close();
-    }
-
-    private void showCreateForm(HttpExchange exchange, Map<String, String> formData, Map<String, String> errors) throws IOException {
-        this.sendHTMLResponse(exchange, this.view.createForm(formData, errors));
     }
 
     public void updateAction(HttpExchange exchange, String uuid) throws IOException {
@@ -72,27 +59,17 @@ public class DoctorController extends AbstractController{
             // before we make decision to save we should validate and be sure that we don't have errors.
             Map<String, String> errors = this.validator.validate(formData);
             if (errors.isEmpty()) {
-                this.updatePutIfSuccess(exchange, uuid, formData);
+                this.service.update(uuid, formData);
+                // redirect back to form page with success message
+                exchange.getResponseHeaders().add("Location", "/doctor/show-doctors");
+                exchange.sendResponseHeaders(303, -1); // 303 = See Other
+                exchange.close();
             } else  {
-                this.showUpdateForm(exchange, uuid, formData, errors);
+                this.sendHTMLResponse(exchange, this.view.updateForm(this.service.findById(uuid), formData, errors));
             }
         } else {
             // first time we send request it will show an empty form without any errors or resubmitted data
-            this.showUpdateForm(exchange, uuid, new HashMap<>(), new HashMap<>());
+            this.sendHTMLResponse(exchange, this.view.updateForm(this.service.findById(uuid), new HashMap<>(), new HashMap<>()));
         }
     }
-
-    private void showUpdateForm(HttpExchange exchange, String uuid, Map<String, String> formData, Map<String, String> errors) throws IOException {
-        this.sendHTMLResponse(exchange, this.view.updateForm(this.service.findById(uuid), formData, errors));
-    }
-
-    private void updatePutIfSuccess(HttpExchange exchange, String uuid, Map<String, String> data) throws IOException {
-        this.service.update(uuid, data);
-        // redirect back to form page with success message
-        exchange.getResponseHeaders().add("Location", "/doctor/show-doctors");
-        exchange.sendResponseHeaders(303, -1); // 303 = See Other
-        exchange.close();
-        //this.sendHTMLResponse(exchange, response);
-    }
-
 }
