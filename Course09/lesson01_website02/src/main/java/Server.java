50a09d7816455c6378182f96e17f6d62d63822db
RSatanovskyi <satcms@hotmail.com>
2026-03-16 19:53:31 +0200

    multipart form to upload photo and static file server

--- Course09/lesson01_website02/src/main/java/Server.java
+++ Course09/lesson01_website02/src/main/java/Server.java
@@ -14,7 +14,7 @@ public class Server {
         HttpServer server = HttpServer.create(new InetSocketAddress(this.port), 0);
 
         // we separated because  StaticFileHandler not allow other routes to execute.
-        // It terminates if file not found everything what is wrong.
+        // It terminates if file not found everything  is wrong. bad implementation so we separate to another context.
         server.createContext("/public", new StaticFileHandler("./public", "/public")); // serve files from ./public folder
         server.createContext("/", new Router());
 
