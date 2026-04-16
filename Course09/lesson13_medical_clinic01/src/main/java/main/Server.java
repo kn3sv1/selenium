774b10d1130168126bda5904b2a326b80a30d66c
RSatanovskyi <satcms@hotmail.com>
2026-04-16 20:01:03 +0300

    integration test for create method

--- Course09/lesson13_medical_clinic01/src/main/java/main/Server.java
+++ Course09/lesson13_medical_clinic01/src/main/java/main/Server.java
@@ -7,19 +7,25 @@ import java.net.InetSocketAddress;
 
 public class Server {
     private int port;
+    private HttpServer server;
 
     public Server(int port) {
         this.port = port;
     }
 
     public void start() throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(this.port), 0);
+        this.server = HttpServer.create(new InetSocketAddress(this.port), 0);
 
         server.createContext("/", new ServerHandler(new Router()));
 
         // Start the server
-        server.setExecutor(null); // default executor
-        server.start();
+        this.server.setExecutor(null); // default executor
+        this.server.start();
         System.out.println("Server started at http://localhost:" + this.port);
+    }
+
+
+    public void stop() {
+        this.server.stop(0);
     }
 }
--- Course09/lesson13_medical_clinic01/src/test/java/controller/DoctorControllerIT.java
+++ Course09/lesson13_medical_clinic01/src/test/java/controller/DoctorControllerIT.java
@@ -1,20 +1,42 @@
 package controller;
 
 import io.restassured.RestAssured;
+import main.Server;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-public class DoctorControllerIT {
+import java.io.File;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class DoctorControllerIT {
+
+    private Server server;
+
+    @BeforeAll
+    void startServer() throws Exception {
+        server = new Server(8081);
+        server.start(); // test port
+
+        baseURI = "http://localhost";
+        port = 8081;
+    }
+
+    @AfterAll
+    void stopServer() {
+        server.stop();
+    }
+
     @Test
     void testHelloEndpoint() {
         RestAssured.baseURI = "http://localhost";
-        RestAssured.port = 8080;
+        RestAssured.port = 8081;
 
         given()
                 .when()
@@ -27,7 +49,7 @@ public class DoctorControllerIT {
     @Test
     void testDoctorsEndpoint() {
         RestAssured.baseURI = "http://localhost";
-        RestAssured.port = 8080;
+        RestAssured.port = 8081;
 
         String body =
                 given()
@@ -45,7 +67,7 @@ public class DoctorControllerIT {
     @Test
     void testDoctorsCSSSelectorEndpoint() {
         RestAssured.baseURI = "http://localhost";
-        RestAssured.port = 8080;
+        RestAssured.port = 8081;
 
         String body =
                 given()
@@ -68,4 +90,21 @@ public class DoctorControllerIT {
         );
 
     }
+
+    @Test
+    void testCreate() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 8081;
+
+        given()
+                .multiPart("file", new File("src/test/resources/images/doctors/test-andreas-integration.png"))
+                .multiPart("name", "AndreasIntegration")
+                .multiPart("surname", "PantazisIntegration")
+                .multiPart("spe", "gynecology")
+            .when()
+                .post("/doctor-create")
+            .then()
+                .statusCode(200);
+    }
+
 }
