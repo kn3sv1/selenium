7b1cf937a25cd4a63d1d902fb57cdb7c5f0a32c9
RSatanovskyi <satcms@hotmail.com>
2026-05-25 21:39:59 +0300

    Course 12 Lesson 2.5 dynamic button event

--- Course12/lesson02_5_ginger_spa_ajax_cats/public/js/controller/CatController.js
+++ Course12/lesson02_5_ginger_spa_ajax_cats/public/js/controller/CatController.js
@@ -1,5 +1,6 @@
 /**
  * @typedef {Object} Cat
+ * @property {string} id
  * @property {string} name
  * @property {number} age
  * @property {string} photo
@@ -40,9 +41,38 @@ export const CatController = {
             <h2>${cat.name}</h2>
             <p>Age: ${cat.age}</p>
             <img height="100" src="${cat.photo}" alt="${cat.name}" />
+            <br /><button class="cat-detail" data-id="${cat.id}">Load Details</button>
         </div>
     `,
         )
-        .join("") + '<div style="clear:both"></div>';
+        .join("") +
+      '<div style="clear:both"></div>' +
+      '<div id="cat-details"></div>';
+
+    // NOW we WANT BUTTON and events for extra ajax from this page.
+    // Can be very big logic here for different AJAX
+    const children = container.querySelectorAll("button.cat-detail");
+    //always we use arrow function => ... because it preserves this
+    children.forEach((element) => {
+      element.addEventListener("click", (e) => {
+        const id = e.target.getAttribute('data-id');
+        //const id = e.target.dataset.id;
+        this.loadCatDetails(id);
+      });
+    });
+
+    // if you want EVENT to another Controller handler - read UNIT 14 of Lesson 2.5
+    // CustomEvent - from one class to another.
+  },
+
+    /**
+   * Fetch details about specific Cat from API.
+   * @returns {Promise<void>}
+   */
+  async loadCatDetails(id) {
+    // console.log("clicked id:" + id);
+    // make ajax request and fill '<div id="cat-details"></div>'
+    const container = document.getElementById('cat-details');
+    container.innerHTML = 'Loading ajax request for ID:' + id;
   },
 };
